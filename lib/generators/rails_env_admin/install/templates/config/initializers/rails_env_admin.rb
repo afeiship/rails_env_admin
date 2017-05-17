f3998d1ae9ecf89646bcae022d37b09af12161f1
afeiship <1290657123@qq.com>
2017-05-17 17:48:53 +0800

    add config

--- lib/generators/rails_env_admin/install/templates/config/initializers/rails_env_admin.rb
+++ lib/generators/rails_env_admin/install/templates/config/initializers/rails_env_admin.rb
@@ -2,3 +2,22 @@
 
 ENV['ADMIN_USER'] = "<%= @username %>"
 ENV['ADMIN_PASSWORD'] = "<%= @password %>"
+
+
+## i18n config:
+Rails.application.config.i18n.available_locales = [:en, :'zh-CN']
+Rails.application.config.i18n.default_locale = (ENV['LOCALE'] || 'zh-CN').to_sym
+
+## timezone:
+Rails.application.config.time_zone = 'Beijing'
+
+## password
+Rails.application.config.action_controller.action_on_unpermitted_parameters = :log
+Rails.application.config.filter_parameters += [:password]
+
+## generators:
+Rails.application.config.generators do |generator|
+  generator.test_framework false
+  generator.helper false
+  generator.assets false
+end
--- lib/rails_env_admin.rb
+++ lib/rails_env_admin.rb
@@ -2,21 +2,4 @@ require "rails_env_admin/version"
 require 'rails_env_admin/engine'
 
 module RailsEnvAdmin
-  # Your code goes here...
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
-
-  class Configuration
-    attr_accessor :example
-
-    def initialize
-      @example = nil
-    end
-  end
 end
