fa4296f2f3f943cc581d646441554aed08fc1897
afeiship <1290657123@qq.com>
2017-07-17 13:38:05 +0800

    issue: styled for env_admin

--- lib/rails_env_admin.rb
+++ lib/rails_env_admin.rb
@@ -2,4 +2,5 @@ require "rails_env_admin/version"
 require 'rails_env_admin/engine'
 
 module RailsEnvAdmin
+    Rails.application.config.assets.precompile += %w( rails_env_admin/rails_env_admin.css )
 end
